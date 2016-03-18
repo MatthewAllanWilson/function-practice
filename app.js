a7c138a4b3369cd661a93d564355d712a3486630
MatthewAllanWilson <mattwils@gmail.com>
2016-03-18 10:21:04 -0700

    Question 3 done.

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 function sum(numberOne, numberTwo) {
   var Total = numberOne + numberTwo;
-  console.log('The sum of ' + numberOne + ' and ' + numberTwo + ' is ' + Total + '.');
+  //console.log('The sum of ' + numberOne + ' and ' + numberTwo + ' is ' + Total + '.');
   return;
 }
-console.log(sum(4,7));
+//console.log(sum(4,7));
 
-function multiply(firstNumber, secondNumber) {
-  var Product = firstNumber * secondNumber;
-  console.log('The product of ' + firstNumber + ' and ' + secondNumber + ' is ' + Product + '.');
+function multiply(numberOne, numberTwo) {
+  var Product = numberOne * numberTwo;
+  //console.log('The product of ' + numberOne + ' and ' + numberTwo + ' is ' + Product + '.');
   return;
 }
-console.log(multiply(4,7));
+//console.log(multiply(4,7));
+
+function sumAndMultiply(numberOne, numberTwo, numberThree) {
+  var Total2 = (sum(numberOne, numberTwo)) + numberThree;
+  var Product2 = (multiply(numberOne, numberTwo)) * numberThree;
+  var questionArray = [Total2, Product2];
+  console.log(numberOne + ' and ' + numberTwo + ' and ' + numberThree + ' sum to ' + questionArray[0] + '.');
+  console.log('The numbers ' + numberOne + ' and ' + numberTwo + ' and ' + numberThree + ' have a product of ' + questionArray[1] + '.');
+  return;
+}
