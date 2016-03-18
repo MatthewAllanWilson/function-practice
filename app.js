000cd115d1a665c12f6d7a5fa762f0f90e5d3fd8
MatthewAllanWilson <mattwils@gmail.com>
2016-03-18 09:21:41 -0700

    Finished Question 2

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ function sum(numberOne, numberTwo) {
   console.log('The sum of ' + numberOne + ' and ' + numberTwo + ' is ' + Total + '.');
   return;
 }
+console.log(sum(4,7));
+
+function multiply(firstNumber, secondNumber) {
+  var Product = firstNumber * secondNumber;
+  console.log('The product of ' + firstNumber + ' and ' + secondNumber + ' is ' + Product + '.');
+  return;
+}
+console.log(multiply(4,7));
